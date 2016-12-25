98540db0eb16f0a6fc890a3ff3a5c34d3b5facda
Bernd Stramm <bernd.stramm@gmail.com>
2016-12-25 09:45:32 -0500

    add subindex

---
+++ berndhs.com/commute/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Bernd's Old Commute</title>
+<body>
+<div align="center">
+<h1>
+<?php echo "Welcome to Bernd's Videos" ?>
+</h1>
+<br>
+Commute inspires me to take the bus:<br>
+<video width="640" height="360" controls>
+  <source src="videos/commute_20140110_640.mp4" type="video/mp4">
+  <source src="videos/commute_20140110_640.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video> 
+<br>
+Loriot's Advent:<br>
+<video width="640" height="360" controls>
+  <source src="videos/Loriot-advent.mp4" type="video/mp4">
+</video>
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10188606; 
+var sc_invisible=1; 
+var sc_security="e6f4ceb4"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify
+analytics" href="http://statcounter.com/shopify/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10188606/0/e6f4ceb4/1/"
+alt="shopify analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+</body>
+</html>
