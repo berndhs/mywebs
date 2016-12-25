b4ef02c7902bf494bbbeffe40e3215e636ba2639
Bernd Stramm <bernd.stramm@gmail.com>
2016-12-25 07:39:10 -0500

    index for berndhs.com

---
+++ berndhs.com/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Bernd's Server</title>
+
+<body>
+<div align="center">
+<h1>
+<?php echo "Welcome to Bernd's Server" ?>
+</h1>
+
+<a  href="oc/oc_index.php">Some Pictures</a>
+<br>
+<a href="commute/">Old Commute</a>
+<br>
+<a href="info.php">Info</a>
+</div>
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10188606; 
+var sc_invisible=1; 
+var sc_security="e6f4ceb4"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify
+analytics" href="http://statcounter.com/shopify/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10188606/0/e6f4ceb4/1/"
+alt="shopify analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+</body>
+</html>
