8076bd913a4a62a7cd51834d2462c3c45c9b41c5
Bernd Stramm <bernd.stramm@gmail.com>
2017-02-12 16:22:39 -0500

    top index fixes

--- samoaweb/index.php
+++ samoaweb/index.php
@@ -3,11 +3,27 @@
 
 <html>
 <head>
+
+<link rel="stylesheet" type="text/css" href="berndhs-styles.css" >
+
 <title><?php echo $_SERVER["HOST_NAME"]; ?> Server</title>
 </head>
 
 <body>
+<script>
+var uag = navigator.userAgent;
+var isMobile = uag.includes("ndroid");
+if (isMobile) {
+  loco = "Mobile";
+} else {
+  loco = "Desk";
+}
+var txt = document.createTextNode(loco);
+document.body.appendChild(txt);
 
+   document.getElementsByName("deskbutton").[0].setAttribute("class",loco);
+   document.getElementsByName("mobilebutton").[0].setAttribute("class",loco);
+</script>
 <script>
  function daRedirect(whereto) {
    var uag = navigator.userAgent;
@@ -15,23 +31,24 @@
    var loco = whereto + "/";
    var pro = document.location.protocol + "//";
    var host = document.location.hostname;
-   var path = pro + host + "/" + loco + "index.php";
+   var filename = (loco == "desk" ? "dindex.php" : "mindex.php");
+   var path = pro + host + "/" + loco + filename;
    document.location.assign(path);
    return path;
  }
 </script>
 
-<dev align="center">
+<div align="center">
 <h1>
-</dev>
+</div>
 
-
+The Index
 <h1>
 <div align="left">
- <button onclick="daRedirect('desk')">Go To Desk</button>
+ <button name="deskbutton" onclick="daRedirect('desk')">Go To Desk</button>
 <br>
 <br>
- <button onclick="daRedirect('mobile')">Go To Mobile</button>
+ <button name="mobilebutton" onclick="daRedirect('mobile')">Go To Mobile</button>
  </div>
  <br>
  <p id="showhere">
